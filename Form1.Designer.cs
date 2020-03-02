21d7ee6f49782931d1c7292cf272dd6ab1a9068d
Csernák Tamás <csermat95@gmail.com>
2020-03-02 11:41:01 +0100

    Test2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,24 +35,26 @@
             this.cbAlbum = new System.Windows.Forms.ComboBox();
             this.pbAlbum = new System.Windows.Forms.PictureBox();
             this.rtbAlbum = new System.Windows.Forms.RichTextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvTrack = new System.Windows.Forms.DataGridView();
             this.tbSearch = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lbURL = new System.Windows.Forms.LinkLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnTest = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbAlbum)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTrack)).BeginInit();
             this.SuspendLayout();
             // 
             // btnAddDisco
             // 
             this.btnAddDisco.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.btnAddDisco.Location = new System.Drawing.Point(374, 467);
-            this.btnAddDisco.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddDisco.Margin = new System.Windows.Forms.Padding(2);
             this.btnAddDisco.Name = "btnAddDisco";
-            this.btnAddDisco.Size = new System.Drawing.Size(184, 56);
+            this.btnAddDisco.Size = new System.Drawing.Size(197, 56);
             this.btnAddDisco.TabIndex = 0;
             this.btnAddDisco.Text = "Add discography";
             this.btnAddDisco.UseVisualStyleBackColor = true;
@@ -61,10 +63,10 @@
             // btnAddURL
             // 
             this.btnAddURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.btnAddURL.Location = new System.Drawing.Point(562, 467);
-            this.btnAddURL.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddURL.Location = new System.Drawing.Point(575, 467);
+            this.btnAddURL.Margin = new System.Windows.Forms.Padding(2);
             this.btnAddURL.Name = "btnAddURL";
-            this.btnAddURL.Size = new System.Drawing.Size(106, 56);
+            this.btnAddURL.Size = new System.Drawing.Size(141, 56);
             this.btnAddURL.TabIndex = 1;
             this.btnAddURL.Text = "Add URL";
             this.btnAddURL.UseVisualStyleBackColor = true;
@@ -73,10 +75,10 @@
             // btnEditSelect
             // 
             this.btnEditSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.btnEditSelect.Location = new System.Drawing.Point(674, 467);
-            this.btnEditSelect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnEditSelect.Location = new System.Drawing.Point(720, 467);
+            this.btnEditSelect.Margin = new System.Windows.Forms.Padding(2);
             this.btnEditSelect.Name = "btnEditSelect";
-            this.btnEditSelect.Size = new System.Drawing.Size(147, 56);
+            this.btnEditSelect.Size = new System.Drawing.Size(173, 56);
             this.btnEditSelect.TabIndex = 2;
             this.btnEditSelect.Text = "Edit Selected";
             this.btnEditSelect.UseVisualStyleBackColor = true;
@@ -87,7 +89,7 @@
             this.cbArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.cbArtist.FormattingEnabled = true;
             this.cbArtist.Location = new System.Drawing.Point(9, 64);
-            this.cbArtist.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbArtist.Margin = new System.Windows.Forms.Padding(2);
             this.cbArtist.Name = "cbArtist";
             this.cbArtist.Size = new System.Drawing.Size(163, 32);
             this.cbArtist.TabIndex = 3;
@@ -97,7 +99,7 @@
             this.cbAlbum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.cbAlbum.FormattingEnabled = true;
             this.cbAlbum.Location = new System.Drawing.Point(182, 64);
-            this.cbAlbum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cbAlbum.Margin = new System.Windows.Forms.Padding(2);
             this.cbAlbum.Name = "cbAlbum";
             this.cbAlbum.Size = new System.Drawing.Size(171, 32);
             this.cbAlbum.TabIndex = 4;
@@ -105,7 +107,7 @@
             // pbAlbum
             // 
             this.pbAlbum.Location = new System.Drawing.Point(370, 208);
-            this.pbAlbum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pbAlbum.Margin = new System.Windows.Forms.Padding(2);
             this.pbAlbum.Name = "pbAlbum";
             this.pbAlbum.Size = new System.Drawing.Size(155, 155);
             this.pbAlbum.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -116,27 +118,27 @@
             // 
             this.rtbAlbum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.rtbAlbum.Location = new System.Drawing.Point(529, 208);
-            this.rtbAlbum.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbAlbum.Margin = new System.Windows.Forms.Padding(2);
             this.rtbAlbum.Name = "rtbAlbum";
-            this.rtbAlbum.Size = new System.Drawing.Size(333, 155);
+            this.rtbAlbum.Size = new System.Drawing.Size(364, 155);
             this.rtbAlbum.TabIndex = 6;
             this.rtbAlbum.Text = "";
             // 
-            // dataGridView1
+            // dgvTrack
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(9, 208);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(344, 315);
-            this.dataGridView1.TabIndex = 7;
+            this.dgvTrack.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTrack.Location = new System.Drawing.Point(9, 208);
+            this.dgvTrack.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvTrack.Name = "dgvTrack";
+            this.dgvTrack.RowTemplate.Height = 24;
+            this.dgvTrack.Size = new System.Drawing.Size(344, 315);
+            this.dgvTrack.TabIndex = 7;
             // 
             // tbSearch
             // 
             this.tbSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
             this.tbSearch.Location = new System.Drawing.Point(9, 162);
-            this.tbSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbSearch.Margin = new System.Windows.Forms.Padding(2);
             this.tbSearch.Name = "tbSearch";
             this.tbSearch.Size = new System.Drawing.Size(344, 29);
             this.tbSearch.TabIndex = 8;
@@ -185,30 +187,47 @@
             this.label4.TabIndex = 12;
             this.label4.Text = "URL (if any) :";
             // 
-            // linkLabel1
+            // lbURL
             // 
-            this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
-            this.linkLabel1.Location = new System.Drawing.Point(510, 410);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(85, 24);
-            this.linkLabel1.TabIndex = 13;
-            this.linkLabel1.TabStop = true;
-            this.linkLabel1.Text = "<Empty>";
+            this.lbURL.AutoSize = true;
+            this.lbURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lbURL.Location = new System.Drawing.Point(510, 410);
+            this.lbURL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbURL.Name = "lbURL";
+            this.lbURL.Size = new System.Drawing.Size(85, 24);
+            this.lbURL.TabIndex = 13;
+            this.lbURL.TabStop = true;
+            this.lbURL.Text = "<Empty>";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnTest
+            // 
+            this.btnTest.BackColor = System.Drawing.Color.Red;
+            this.btnTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnTest.Location = new System.Drawing.Point(854, 11);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(39, 40);
+            this.btnTest.TabIndex = 14;
+            this.btnTest.Text = "T";
+            this.btnTest.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 546);
-            this.Controls.Add(this.linkLabel1);
+            this.ClientSize = new System.Drawing.Size(904, 546);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.lbURL);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tbSearch);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvTrack);
             this.Controls.Add(this.rtbAlbum);
             this.Controls.Add(this.pbAlbum);
             this.Controls.Add(this.cbAlbum);
@@ -216,11 +235,11 @@
             this.Controls.Add(this.btnEditSelect);
             this.Controls.Add(this.btnAddURL);
             this.Controls.Add(this.btnAddDisco);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pbAlbum)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTrack)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -235,13 +254,15 @@
         private System.Windows.Forms.ComboBox cbAlbum;
         private System.Windows.Forms.PictureBox pbAlbum;
         private System.Windows.Forms.RichTextBox rtbAlbum;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvTrack;
         private System.Windows.Forms.TextBox tbSearch;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel lbURL;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnTest;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,11 +15,18 @@ namespace Beadandó200212
     {
         public Form1()
         {
-            
+            openFileDialog1 = new OpenFileDialog()
+            {
+                FileName = "Select a database",
+                Filter = "Database (*.mdb)|*.mdb",
+                Title = "Select database path"
+            };
+
+
 
             InitializeComponent();
-            
-            TestInit();
+            btnTest.Click += new EventHandler(btnTest_Click);
+            Controls.Add(btnTest);
         }
 
         private void btnAddDisco_Click(object sender, EventArgs e)
@@ -42,18 +49,82 @@ namespace Beadandó200212
             MessageBox.Show("Soon(TM).","Test");
         }
 
-        private void TestInit()
+        private void TestInit(string filePath)
         {
-            pbAlbum.Image = Beadandó200212.Properties.Resources.homv;
-            /*
+            
             string connectionString;
+            string sql;
             OleDbConnection cnn;
 
-            connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\Users\csernakt\Documents\Beadandó.accdb;Persist Security Info=False;";
+            connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source="+filePath+";Persist Security Info=False;";
             cnn = new OleDbConnection(connectionString);
             cnn.Open();
+            OleDbCommand cmd = cnn.CreateCommand();
+            cmd.Connection = cnn;
+            cmd.CommandText = "SELECT TOP 1 artist FROM Albums;"; //Artist
+            OleDbDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                cbArtist.Items.Add(dr[0]);
+            }
+            dr.Close();
+
+            cmd.CommandText = "SELECT TOP 1 title FROM Albums;"; //Albums
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                cbAlbum.Items.Add(dr[0]);
+            }
+            dr.Close();
+
+            var CommandTextStr = "SELECT TOP 1 title,length FROM Tracks;"; //Tracks
+            var dataAdapter = new OleDbDataAdapter(CommandTextStr, connectionString);
+
+            var commandBuilder = new OleDbCommandBuilder(dataAdapter);
+            var ds = new DataSet();
+            dataAdapter.Fill(ds);
+            dgvTrack.ReadOnly = true;
+            dgvTrack.DataSource = ds.Tables[0];
+            
+
+            cmd.CommandText = "SELECT TOP 1 url FROM Tracks;"; //URL
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lbURL.Text="https://youtu.be/"+dr.GetString(0);
+            }
+            dr.Close();
+
+            cmd.CommandText = "SELECT TOP 1 id FROM Albums;"; //PictureBox
+            dr = cmd.ExecuteReader();
+            var picturename="";
+            while (dr.Read())
+            {
+                picturename = dr.GetString(0);
+            }
+            pbAlbum.Image = (Image)Beadandó200212.Properties.Resources.ResourceManager.GetObject(picturename);
+            dr.Close();
+
             cnn.Close();
-            */
+            
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFileDialog1.FileName;
+                    //MessageBox.Show(filePath);
+                    TestInit(filePath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
         }
     }
 }
